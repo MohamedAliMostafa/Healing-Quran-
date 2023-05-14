8e0fb8e1afcfc4d5ce40c7d149b8b7d856ced352
Lenovo <https://github.com/MohamedAliMostafa/islamiAPP.git>
2023-05-14 15:47:04 +0200

    sebha

--- lib/Provider/Mytheme.dart
+++ lib/Provider/Mytheme.dart
@@ -1,14 +1,35 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Mytheme extends ChangeNotifier
 {
+  Mytheme()
+  {
+    getThemeAtInit();
+  }
+
   ThemeMode themeData=ThemeMode.light;
 
-  Void? Changetheme(ThemeMode data)
-  {
+  Future<Void?> Changetheme(ThemeMode data)
+  async {
     themeData=data;
     notifyListeners();
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setBool("is_dark", themeData == ThemeMode.dark);
   }
+  getThemeAtInit() async{
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    bool? isDarkTheme =
+    sharedPreferences.getBool("is_dark");
+    if (isDarkTheme != null && isDarkTheme) {
+      themeData = ThemeMode.dark;
+    } else {
+      themeData = ThemeMode.light;
+    }
+    notifyListeners();
+  }
+
+
 }
